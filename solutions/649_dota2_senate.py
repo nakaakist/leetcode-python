cbb6e59c432e3be888bb4cddce387abc9accee63
Nariyuki Saito <toshincompos@gmail.com>
2023-07-08 15:47:49 +0900

    Fix 649

--- solutions/649_dota2_senate.py
+++ solutions/649_dota2_senate.py
@@ -1,36 +1,30 @@
-from collections import Counter
+from collections import Counter, deque
 
 
 class Solution:
     def predictPartyVictory(self, senate: str) -> str:
         live_counts = Counter(senate)
         can_kill_counts = Counter()
-        chars = [*senate]
-        i = 0
-        while True:
-            c = chars[i]
+        chars = deque(senate)
+        while live_counts["D"] and live_counts["R"]:
+            c = chars.popleft()
 
             if c == "R":
                 if can_kill_counts["R"] > 0:
-                    chars[i] = ""
                     live_counts["R"] -= 1
                     can_kill_counts["R"] -= 1
-                elif live_counts["D"] == 0:
-                    return "Radiant"
                 else:
                     can_kill_counts["D"] += 1
+                    chars.append(c)
             elif c == "D":
-                print(can_kill_counts, live_counts)
                 if can_kill_counts["D"] > 0:
-                    chars[i] = ""
                     live_counts["D"] -= 1
                     can_kill_counts["D"] -= 1
-                elif live_counts["R"] == 0:
-                    return "Dire"
                 else:
                     can_kill_counts["R"] += 1
+                    chars.append(c)
 
-            i = (i + 1) % len(senate)
+        return "Dire" if live_counts["D"] else "Radiant"
 
 
 s = Solution()
