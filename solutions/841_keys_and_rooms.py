6bd15b06f6e994254644ceac0dd6992789464807
Nariyuki Saito <toshincompos@gmail.com>
2023-07-15 23:15:19 +0900

    Fix 841

--- solutions/841_keys_and_rooms.py
+++ solutions/841_keys_and_rooms.py
@@ -4,23 +4,19 @@ from typing import List
 
 class Solution:
     def canVisitAllRooms(self, rooms: List[List[int]]) -> bool:
-        keys = defaultdict(bool)
-        keys[0] = True
+        seen = [False] * len(rooms)
+        seen[0] = True
         stack = [0]
 
         while stack:
             r = stack.pop()
             ks = rooms[r]
             for k in ks:
-                if not keys[k]:
-                    keys[k] = True
+                if not seen[k]:
+                    seen[k] = True
                     stack.append(k)
 
-        for r in range(len(rooms)):
-            if keys[r] == False:
-                return False
-
-        return True
+        return all(seen)
 
 
 print(Solution().canVisitAllRooms([[1, 3], [3, 0, 1], [2], [0]]))
