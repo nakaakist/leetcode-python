13e4acc0f307cf9b18d0b293e4321913b94a0eea
Nariyuki Saito <toshincompos@gmail.com>
2023-07-15 21:42:18 +0900

    Add 450

---
+++ solutions/450_delete_node_in_a_bst.py
@@ -0,0 +1,45 @@
+from typing import Optional
+
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def successor(self, node: TreeNode) -> int:
+        node = node.right
+        while node.left:
+            node = node.left
+
+        return node.val
+
+    def predecessor(self, node: TreeNode) -> int:
+        node = node.left
+        while node.right:
+            node = node.right
+
+        return node.val
+
+    def deleteNode(self, root: Optional[TreeNode], key: int) -> Optional[TreeNode]:
+        if not root:
+            return None
+
+        if key > root.val:
+            root.right = self.deleteNode(root.right, key)
+        elif key < root.val:
+            root.left = self.deleteNode(root.left, key)
+        else:
+            if not (root.left) and not root.right:
+                return None
+            elif root.right:
+                root.val = self.successor(root)
+                root.right = self.deleteNode(root.right, root.val)
+            else:
+                root.val = self.predecessor(root)
+                root.left = self.deleteNode(root.left, root.val)
+
+        return root
