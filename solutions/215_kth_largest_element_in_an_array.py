73381f73bbcea34dcccd4c90492d6ef781430270
Nariyuki Saito <toshincompos@gmail.com>
2023-07-20 02:22:10 +0900

    Fix 215

--- solutions/215_kth_largest_element_in_an_array.py
+++ solutions/215_kth_largest_element_in_an_array.py
@@ -1,16 +1,28 @@
-import heapq
+import random
 from typing import List
 
 
 class Solution:
     def findKthLargest(self, nums: List[int], k: int) -> int:
-        heap = []
-        for num in nums:
-            heapq.heappush(heap, num)
-            if len(heap) > k:
-                heapq.heappop(heap)
+        def quick_select(nums, k):
+            pivot = random.choice(nums)
+            left, mid, right = [], [], []
+            for n in nums:
+                if n < pivot:
+                    left.append(n)
+                elif n == pivot:
+                    mid.append(n)
+                else:
+                    right.append(n)
 
-        return heap[0]
+            if len(right) >= k:
+                return quick_select(right, k)
+            elif len(right) + len(mid) >= k:
+                return pivot
+            else:
+                return quick_select(left, k - len(right) - len(mid))
+
+        return quick_select(nums, k)
 
 
 print(Solution().findKthLargest([1, 2, 3, 4], 2))
