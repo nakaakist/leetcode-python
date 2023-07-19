8f98baf01cd745a0529a2f2df0ee3ee4f717f2d0
Nariyuki Saito <toshincompos@gmail.com>
2023-07-20 00:49:47 +0900

    Fix 1926

--- solutions/1926_nearest_exit_from_engrance_in_maze.py
+++ solutions/1926_nearest_exit_from_engrance_in_maze.py
@@ -5,36 +5,23 @@ from typing import List
 class Solution:
     def nearestExit(self, maze: List[List[str]], entrance: List[int]) -> int:
         n_rows, n_cols = len(maze), len(maze[0])
-        visited = defaultdict(lambda: defaultdict(lambda: False))
-        visited[entrance[0]][entrance[1]] = True
+        maze[entrance[0]][entrance[1]] = "+"
         queue = deque([(entrance, 0)])
 
-        def visit_if_possible(x, y, step):
-            if visited[x][y]:
-                return False
-
-            if (
-                (x >= 0 and x < n_rows)
-                and (y >= 0 and y < n_cols)
-                and maze[x][y] == "."
-            ):
-                queue.append(((x, y), step + 1))
-                visited[x][y] = True
-                if x == 0 or x == n_rows - 1 or y == 0 or y == n_cols - 1:
-                    return True
-
-            return False
-
         while queue:
-            (x, y), step = queue.popleft()
-            print(x,y)
-            if (
-                visit_if_possible(x - 1, y, step)
-                or visit_if_possible(x + 1, y, step)
-                or visit_if_possible(x, y - 1, step)
-                or visit_if_possible(x, y + 1, step)
-            ):
-                return step + 1
+            (_x, _y), step = queue.popleft()
+            for dx, dy in [(1, 0), (-1, 0), (0, 1), (0, -1)]:
+                x, y = _x + dx, _y + dy
+                if x < 0 or x >= n_rows or y < 0 or y >= n_cols:
+                    continue
+                if maze[x][y] == "+":
+                    continue
+
+                if x == 0 or x == n_rows - 1 or y == 0 or y == n_cols - 1:
+                    return step + 1
+
+                maze[x][y] = "+"
+                queue.append(((x, y), step + 1))
 
         return -1
 
