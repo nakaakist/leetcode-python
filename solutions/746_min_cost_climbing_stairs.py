cd008e8d0b85ed9c39ae1947cc906ae291af29e3
Nariyuki Saito <toshincompos@gmail.com>
2023-07-23 16:28:25 +0900

    Fix 746

--- solutions/746_min_cost_climbing_stairs.py
+++ solutions/746_min_cost_climbing_stairs.py
@@ -3,16 +3,14 @@ from typing import List
 
 class Solution:
     def minCostClimbingStairs(self, cost: List[int]) -> int:
-        memo = {0: 0, 1: 0}
+        prev1, prev2 = 0, 0
 
-        def min_cost(i):
-            if i in memo:
-                return memo[i]
+        for i in range(2, len(cost) + 1):
+            curr = min(prev1 + cost[i - 2], prev2 + cost[i - 1])
+            prev1 = prev2
+            prev2 = curr
 
-            memo[i] = min(min_cost(i - 2) + cost[i - 2], min_cost(i - 1) + cost[i - 1])
-            return memo[i]
-
-        return min_cost(len(cost))
+        return curr
 
 
 print(Solution().minCostClimbingStairs([1, 100, 1, 1, 1, 100, 1, 1, 100, 1]))
