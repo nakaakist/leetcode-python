7f4651ceb2b5bd554710e75d20ebc51f718b280c
Nariyuki Saito <toshincompos@gmail.com>
2023-07-24 21:56:24 +0900

    Fix 790

--- solutions/790_domino_and_tromino_tiling.py
+++ solutions/790_domino_and_tromino_tiling.py
@@ -2,29 +2,17 @@ class Solution:
     MOD = 10**9 + 7
 
     def numTilings(self, n: int) -> int:
-        memo = {1: 1, 2: 2}
-        memo_half_filled = {1: 0, 2: 2}
+        memo = {0: 0, 1: 1, 2: 2}
+        memo_half_filled = {0: 0, 1: 0, 2: 2}
 
-        def num_half_filled_tilings(n):
-            if n in memo_half_filled:
-                return memo_half_filled[n]
-
-            memo_half_filled[n] = (
-                num_half_filled_tilings(n - 1) + 2 * num_tilings(n - 2)
-            ) % self.MOD
-
-            return memo_half_filled[n]
-
-        def num_tilings(n):
-            if n in memo:
-                return memo[n]
-
-            memo[n] = (
-                num_tilings(n - 1) + num_tilings(n - 2) + num_half_filled_tilings(n - 1)
-            ) % self.MOD
+        if n <= 2:
             return memo[n]
 
-        return num_tilings(n)
+        for i in range(3, n + 1):
+            memo_half_filled[i] = (memo_half_filled[i - 1] + 2 * memo[i - 2]) % self.MOD
+            memo[i] = (memo[i - 1] + memo[i - 2] + memo_half_filled[i - 1]) % self.MOD
+
+        return memo[n]
 
 
-print(Solution().numTilings(1))
+print(Solution().numTilings(1000))
