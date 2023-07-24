13102d3d975344cada7403722256d624dd49b07b
Nariyuki Saito <toshincompos@gmail.com>
2023-07-25 01:04:30 +0900

    Fix 714

--- solutions/714_best_time_to_buy_and_sell_stock_with_transaction_fee.py
+++ solutions/714_best_time_to_buy_and_sell_stock_with_transaction_fee.py
@@ -4,14 +4,15 @@ from typing import List
 class Solution:
     def maxProfit(self, prices: List[int], fee: int) -> int:
         size = len(prices)
-        hold = [-prices[0]] * (size)
-        free = [0] * (size)
+        hold = -prices[0]
+        free = 0
 
         for i in range(1, size):
-            hold[i] = max(hold[i - 1], free[i - 1] - prices[i])
-            free[i] = max(free[i - 1], prices[i] - fee + hold[i - 1])
+            tmp = hold
+            hold = max(tmp, free - prices[i])
+            free = max(free, prices[i] - fee + tmp)
 
-        return free[size - 1]
+        return free
 
 
 print(Solution().maxProfit(prices=[1, 3, 2, 8, 4, 9], fee=2))
