70a1579825fab5f30fd3fb798586bc2a1beb203b
Nariyuki Saito <toshincompos@gmail.com>
2023-07-25 22:15:17 +0900

    Add 338

---
+++ solutions/338_counting_bits.py
@@ -0,0 +1,12 @@
+from typing import List
+
+
+class Solution:
+    def countBits(self, n: int) -> List[int]:
+        ans = [0] * (n + 1)
+        for x in range(1, n + 1):
+            ans[x] = ans[x >> 1] + (x & 1)
+        return ans
+
+
+print(Solution().countBits(15))
