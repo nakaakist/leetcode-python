482fea36dae2e348e4ce71919f0ccacd59647272
Nariyuki Saito <toshincompos@gmail.com>
2023-08-26 22:34:43 +0900

    Fix 130

--- solutions/130_surrounded_regions.py
+++ solutions/130_surrounded_regions.py
@@ -6,38 +6,14 @@ class Solution:
         n_row = len(board)
         n_col = len(board[0])
 
-        islands = []
+        def dfs(i, j):
+            if board[i][j] == "X" or board[i][j] == "E":
+                return
 
-        for i in range(n_row):
-            for j in range(n_col):
-                if board[i][j] == "X" or board[i][j] == "V":
-                    continue
-
-                stack = [(i, j)]
-                should_be_captured = True
-                while stack:
-                    k, l = stack.pop()
-                    board[k][l] = "V"
-                    if k == 0 or k == n_row - 1 or l == 0 or l == n_col - 1:
-                        should_be_captured = False
-                    for dv, dh in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
-                        m, n = k + dv, l + dh
-                        if (
-                            m >= 0
-                            and m < n_row
-                            and n >= 0
-                            and n < n_col
-                            and board[m][n] == "O"
-                        ):
-                            stack.append((m, n))
-                islands.append((i, j, should_be_captured))
-
-        while islands:
-            i, j, should_be_captured = islands.pop()
             stack = [(i, j)]
             while stack:
                 k, l = stack.pop()
-                board[k][l] = "X" if should_be_captured else "O"
+                board[k][l] = "E"
                 for dv, dh in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
                     m, n = k + dv, l + dh
                     if (
@@ -45,6 +21,20 @@ class Solution:
                         and m < n_row
                         and n >= 0
                         and n < n_col
-                        and board[m][n] == "V"
+                        and board[m][n] == "O"
                     ):
                         stack.append((m, n))
+
+        for i in range(n_row):
+            for j in [0, n_col - 1]:
+                dfs(i, j)
+        for i in [0, n_row - 1]:
+            for j in range(n_col):
+                dfs(i, j)
+        for i in range(n_row):
+            for j in range(n_col):
+                v = board[i][j]
+                if v == 'O':
+                    board[i][j] = 'X'
+                elif v == 'E':
+                    board[i][j] = 'O'
