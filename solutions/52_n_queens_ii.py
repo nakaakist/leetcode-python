cbf3bd13eac3be1fccd5754607b456d0fd07dbd8
Nariyuki Saito <toshincompos@gmail.com>
2023-09-09 00:18:02 +0900

    Fix 52

--- solutions/52_n_queens_ii.py
+++ solutions/52_n_queens_ii.py
@@ -5,43 +5,30 @@ class Solution:
     def totalNQueens(self, n: int) -> int:
         ans = 0
 
-        def idx_to_grid(idx):
-            return divmod(idx, n)
-
-        def place(i, j, placed, filled_rows, filled_cols):
-            for k, l in placed:
+        def place(i: int, j: int, placed: List[int]):
+            for k, l in enumerate(placed):
                 if k == i or l == j:
                     return False
                 if abs(k - i) == abs(l - j):
                     return False
-            placed.append((i, j))
-            filled_rows.add(i)
-            filled_cols.add(j)
+            placed.append(j)
             return True
 
-        def remove(i, j, placed, filled_rows, filled_cols):
+        def remove(placed):
             placed.pop()
-            filled_rows.remove(i)
-            filled_cols.remove(j)
 
-        def backtrack(idx, placed, filled_rows, filled_cols, n_to_place):
+        def backtrack(i: int, placed: List[int]):
             nonlocal ans
-            i, j = idx_to_grid(idx)
-            if n_to_place == 0:
+            if i == n:
                 ans += 1
                 return
-            if idx == n**2:
-                return
 
-            backtrack(idx + 1, placed, filled_rows, filled_cols, n_to_place)
-            if i in filled_rows or j in filled_cols:
-                return
+            for j in range(n):
+                if place(i, j, placed):
+                    backtrack(i + 1, placed)
+                    remove(placed)
 
-            if place(i, j, placed, filled_rows, filled_cols):
-                backtrack(idx + 1, placed, filled_rows, filled_cols, n_to_place - 1)
-                remove(i, j, placed, filled_rows, filled_cols)
-
-        backtrack(0, [], set(), set(), n)
+        backtrack(0, [])
 
         return ans
 
