e6bdd1c989b006308caf60996872b87e9d0c7af1
Nariyuki Saito <toshincompos@gmail.com>
2023-09-09 00:07:54 +0900

    Fix 52

--- solutions/52_n_queens_ii.py
+++ solutions/52_n_queens_ii.py
@@ -1,50 +1,30 @@
+from typing import List
+
+
 class Solution:
     def totalNQueens(self, n: int) -> int:
-        placed = [[False] * n for _ in range(n)]
-        attacked = [[False] * n for _ in range(n)]
         ans = 0
 
         def idx_to_grid(idx):
             return divmod(idx, n)
 
-        def place(i, j, placed, attacked):
-            if attacked[i][j]:
-                return False
-            for k in range(n):
-                if placed[k][j]:  # vertical
+        def place(i, j, placed, filled_rows, filled_cols):
+            for k, l in placed:
+                if k == i or l == j:
                     return False
-                if placed[i][k]:  # horizontal
+                if abs(k - i) == abs(l - j):
                     return False
-                if (
-                    j + k - i >= 0 and j + k - i < n and placed[k][j + k - i]
-                ):  # diagonal
-                    return False
-                if (
-                    j - k + i >= 0 and j - k + i < n and placed[k][j - k + i]
-                ):  # diagonal
-                    return False
-            placed[i][j] = True
-            for k in range(n):
-                attacked[k][j] = True
-                attacked[i][k] = True
-                if j + k - i >= 0 and j + k - i < n:
-                    attacked[k][j + k - i] = True
-                if j - k + i >= 0 and j - k + i < n:
-                    attacked[k][j - k + i] = True
-
+            placed.append((i, j))
+            filled_rows.add(i)
+            filled_cols.add(j)
             return True
 
-        def remove(i, j, placed, attacked):
-            placed[i][j] = False
-            for k in range(n):
-                attacked[k][j] = False
-                attacked[i][k] = False
-                if j + k - i >= 0 and j + k - i < n:
-                    attacked[k][j + k - i] = False
-                if j - k + i >= 0 and j - k + i < n:
-                    attacked[k][j - k + i] = False
+        def remove(i, j, placed, filled_rows, filled_cols):
+            placed.pop()
+            filled_rows.remove(i)
+            filled_cols.remove(j)
 
-        def backtrack(idx, placed, attacked, n_to_place):
+        def backtrack(idx, placed, filled_rows, filled_cols, n_to_place):
             nonlocal ans
             i, j = idx_to_grid(idx)
             if n_to_place == 0:
@@ -53,13 +33,15 @@ class Solution:
             if idx == n**2:
                 return
 
-            place_res = place(i, j, placed, attacked)
-            if place_res:
-                backtrack(idx + 1, placed, attacked, n_to_place - 1)
-                remove(i, j, placed, attacked)
-            backtrack(idx + 1, placed, attacked, n_to_place)
+            backtrack(idx + 1, placed, filled_rows, filled_cols, n_to_place)
+            if i in filled_rows or j in filled_cols:
+                return
 
-        backtrack(0, placed, attacked, n)
+            if place(i, j, placed, filled_rows, filled_cols):
+                backtrack(idx + 1, placed, filled_rows, filled_cols, n_to_place - 1)
+                remove(i, j, placed, filled_rows, filled_cols)
+
+        backtrack(0, [], set(), set(), n)
 
         return ans
 
@@ -72,3 +54,4 @@ print(Solution().totalNQueens(5))
 print(Solution().totalNQueens(6))
 print(Solution().totalNQueens(7))
 print(Solution().totalNQueens(8))
+print(Solution().totalNQueens(9))
