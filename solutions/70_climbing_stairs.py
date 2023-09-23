9769143480f75a110a33f4b108213915ad8a9c5d
Nariyuki Saito <toshincompos@gmail.com>
2023-09-24 01:01:22 +0900

    Fix 70

--- solutions/70_climbing_stairs.py
+++ solutions/70_climbing_stairs.py
@@ -1,22 +1,12 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        memo = [-1] * (n + 1)
+        c, p = 1, 0
+        for _ in range(n):
+            c, p = c + p, c
 
-        def _count(m: int):
-            if m == 0:
-                return 1
-            elif m < 0:
-                return 0
-
-            if memo[m] >= 0:
-                return memo[m]
-
-            cnt = _count(m - 1) + _count(m - 2)
-            memo[m] = cnt
-
-            return cnt
-
-        return _count(n)
+        return c
 
 
+print(Solution().climbStairs(1))
 print(Solution().climbStairs(3))
+print(Solution().climbStairs(2))
