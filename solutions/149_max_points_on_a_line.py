f8d94a262f5f27708ed3d74493f5749748c390c9
Nariyuki Saito <toshincompos@gmail.com>
2023-09-24 00:51:52 +0900

    Fix 149

--- solutions/149_max_points_on_a_line.py
+++ solutions/149_max_points_on_a_line.py
@@ -1,22 +1,21 @@
+from collections import defaultdict
+from math import atan2
 from typing import List
 
 
 class Solution:
     def maxPoints(self, points: List[List[int]]) -> int:
-        def is_on_line(p1: List[int], p2: List[int], p_test: List[int]) -> int:
-            return (p_test[0] - p1[0]) * (p_test[1] - p2[1]) - (p_test[0] - p2[0]) * (
-                p_test[1] - p1[1]
-            ) == 0
-
         l_p = len(points)
         ans = 1
         for i in range(l_p):
-            for j in range(i + 1, l_p):
-                n_on_line = 0
-                for k in range(l_p):
-                    if is_on_line(points[i], points[j], points[k]):
-                        n_on_line += 1
-                ans = max(ans, n_on_line)
+            counts = defaultdict(lambda: 1)
+            for j in range(l_p):
+                if i == j:
+                    continue
+                rad = atan2(points[j][0] - points[i][0], points[j][1] - points[i][1])
+                counts[rad] += 1
+            if len(counts) > 0:
+                ans = max(ans, max(counts.values()))
 
         return ans
 
