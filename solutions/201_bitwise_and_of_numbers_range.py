f75405da8d2e05fc53b3d3ea6dacb4e1ee1ea2f1
Nariyuki Saito <toshincompos@gmail.com>
2023-09-23 17:22:32 +0900

    Fix 201

--- solutions/201_bitwise_and_of_numbers_range.py
+++ solutions/201_bitwise_and_of_numbers_range.py
@@ -1,18 +1,12 @@
 class Solution:
     def rangeBitwiseAnd(self, left: int, right: int) -> int:
-        ans = 0
-        cnt = right
+        shift = 0
+        while left < right:
+            left >>= 1
+            right >>= 1
+            shift += 1
 
-        base = 1
-        while cnt > 0:
-            dl, ml = divmod(left, 2 * base)
-            dr, mr = divmod(right, 2 * base)
-            if dl == dr and (ml >= base and mr >= base):
-                ans |= base
-            cnt >>= 1
-            base *= 2
-
-        return ans
+        return left << shift
 
 
 print(Solution().rangeBitwiseAnd(left=5, right=7))
