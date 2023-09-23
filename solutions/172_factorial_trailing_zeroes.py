3c2bd774621e915ad551a58102c6d67d211f10f7
Nariyuki Saito <toshincompos@gmail.com>
2023-09-23 17:56:36 +0900

    Add 172

--- solutions/172_factorial_trailing_zeroes.py
+++ solutions/172_factorial_trailing_zeroes.py
@@ -1,21 +1,16 @@
 class Solution:
     def trailingZeroes(self, n: int) -> int:
-        n_5 = 0
-        n_2 = 0
-
-        def count_factor(i: int, f: int) -> int:
+        def count_factor(f: int) -> int:
+            _f = f
             ans = 0
-            while i % f == 0:
-                ans += 1
-                i //= f
+            while n >= _f:
+                ans += n // _f
+                _f *= f
+
             return ans
 
-        for i in range(1, n + 1):
-            n_5 += count_factor(i, 5)
-            n_2 += count_factor(i, 2)
-
-        return min(n_5, n_2)
+        return count_factor(5)
 
 
 print(Solution().trailingZeroes(3))
-print(Solution().trailingZeroes(5))
+print(Solution().trailingZeroes(30))
