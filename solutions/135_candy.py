9fd6edd41d3d31b5eda47706a9191b8bd1b33076
Nariyuki Saito <toshincompos@gmail.com>
2023-10-19 01:14:21 +0900

    Fix 135

--- solutions/135_candy.py
+++ solutions/135_candy.py
@@ -3,29 +3,30 @@ from typing import List
 
 class Solution:
     def candy(self, ratings: List[int]) -> int:
-        candies = [1] * len(ratings)
-        for i in range(1, len(ratings)):
-            curr_r = ratings[i]
-            prev_r = ratings[i - 1]
-            prev_c = candies[i - 1]
-            if curr_r > prev_r:
-                candies[i] = prev_c + 1
+        n = len(ratings)
+        steps_to_down = [0] * n
+        curr = 0
+        for i in range(n - 2, -1, -1):
+            if ratings[i] > ratings[i + 1]:
+                curr += 1
             else:
-                candies[i] = 1
+                curr = 0
+            steps_to_down[i] = curr
 
-            for j in range(i, 0, -1):
-                if (candies[j - 1] <= candies[j]) and (ratings[j - 1] > ratings[j]):
-                    print(candies)
-                    candies[j - 1] += 1
-                else:
-                    break
+        curr = steps_to_down[0] + 1
+        ans = curr
+        for i in range(1, n):
+            if ratings[i] > ratings[i - 1]:
+                curr = max(curr + 1, steps_to_down[i] + 1)
+            else:
+                curr = steps_to_down[i] + 1
+            ans += curr
 
-        print(candies)
-        return sum(candies)
+        return ans
 
 
 print(Solution().candy([1, 0, 2]))
 print(Solution().candy([1, 2, 2]))
 print(Solution().candy([1, 3, 2, 2, 1]))
 print(Solution().candy([1, 2, 3, 1, 0]))
-# print(Solution().candy(list(range(12000, 0, -1))))
+print(Solution().candy(list(range(12000, 0, -1))))
